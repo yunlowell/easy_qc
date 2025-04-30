804e813f1981dbdaf21d45c3852c7e6615ac0bd7
Junghyun Kim <hyunheee@swu.ac.kr>
2025-04-30 19:19:38 +0900

    홈 화면 생성 및 액티비티 연결

--- easyqc3/app/src/main/java/com/example/easyqc3/HomeActivity.kt
+++ easyqc3/app/src/main/java/com/example/easyqc3/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.easyqc3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val gotoMeasuringBtn = findViewById<ImageView>(R.id.gotoMeasuringBtn)
+        val gotoStandardBtn = findViewById<ImageView>(R.id.gotoStandardBtn)
+        val gotoHistoryBtn = findViewById<ImageView>(R.id.gotoHistoryBtn)
+
+        // 화면 전환 page들
+        gotoMeasuringBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+
+        gotoStandardBtn.setOnClickListener {
+            val intent = Intent(this, StandardActivity::class.java)
+            startActivity(intent)
+        }
+
+        gotoHistoryBtn.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
