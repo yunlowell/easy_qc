2b56f8764b289d3c8049e149e12c94864698d357
cha-kyungho <can.khcha@gmail.com>
2025-05-01 19:02:08 +0900

    Camera Calibration 화면 추가
    Camera Calibration 화면 추가

--- easyqc3/app/src/main/java/com/example/easyqc3/MainActivity.kt
+++ easyqc3/app/src/main/java/com/example/easyqc3/MainActivity.kt
@@ -1,84 +1,6 @@
 package com.example.easyqc3
 
-/*  opencv Camera View만 나오는 sample
-import android.os.Bundle
-import android.util.Log
-import android.view.SurfaceView
-import android.view.WindowManager
-import android.widget.Toast
-import org.opencv.android.CameraActivity
-import org.opencv.android.CameraBridgeViewBase
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2
-import org.opencv.android.OpenCVLoader
-import org.opencv.core.Mat
-
-class MainActivity : CameraActivity(), CvCameraViewListener2 {
-
-    companion object {
-        private const val TAG = "OCVSample::Activity"
-    }
-
-    private var mOpenCvCameraView: CameraBridgeViewBase? = null
-
-    init {
-        Log.i(TAG, "Instantiated new ${this::class.java}")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i(TAG, "called onCreate")
-
-        // OpenCV 로드
-        if (OpenCVLoader.initLocal()) {
-            Log.i(TAG, "OpenCV loaded successfully")
-        } else {
-            Log.e(TAG, "OpenCV initialization failed!")
-            Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        setContentView(R.layout.activity_main)
-
-        mOpenCvCameraView = findViewById(R.id.activity_surface_view)
-        mOpenCvCameraView?.visibility = SurfaceView.VISIBLE
-        mOpenCvCameraView?.setCvCameraViewListener(this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mOpenCvCameraView?.enableView()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mOpenCvCameraView?.disableView()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mOpenCvCameraView?.disableView()
-    }
-
-    override fun getCameraViewList(): List<CameraBridgeViewBase> {
-        return listOfNotNull(mOpenCvCameraView)
-    }
-
-    override fun onCameraViewStarted(width: Int, height: Int) {
-        // 필요하면 초기화 작업
-    }
-
-    override fun onCameraViewStopped() {
-        // 필요하면 정리 작업
-    }
-
-    override fun onCameraFrame(inputFrame: CvCameraViewFrame): Mat {
-        return inputFrame.rgba()
-    }
-}
-*/
-
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -94,10 +16,6 @@ import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.view.SurfaceView
 import android.widget.Button
 
 
@@ -157,13 +75,13 @@ class MainActivity : CameraActivity(), CvCameraViewListener2 {
         mOpenCvCameraView?.visibility = CameraBridgeViewBase.VISIBLE
         mOpenCvCameraView?.setCvCameraViewListener(this)
 
-
+/*
         btnRGB = this.findViewById(R.id.btn1);
         btnRGB.setOnClickListener {
             this.mViewMode = VIEW_MODE_RGBA
             Toast.makeText(this, "RGBA 모드", Toast.LENGTH_SHORT).show()
         }
-
+*/
         btnGRAY= this.findViewById(R.id.btn2);
         btnGRAY.setOnClickListener {
             this.mViewMode = VIEW_MODE_GRAY
@@ -178,6 +96,8 @@ class MainActivity : CameraActivity(), CvCameraViewListener2 {
         btnCameraCalibration.setOnClickListener {
 
             Toast.makeText(this, "Camera Calibration", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, CameraCalibrationActivity::class.java)
+            startActivity(intent)
         }
 
     }
